a2c7c63b398b802d0ab06de04e4ce2d3f03c27c7
Franky Vong <franky.vong@gmail.com>
2024-08-26 22:19:55 +1000

    update

--- app/simplelbo/layout.js
+++ app/simplelbo/layout.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Header from "../../components/LessonHeader";
 
 export default function SimpleLBOLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <Suspense><main className="flex-grow">{children}</main>
+      </Suspense>
+      
       {/* <Footer /> */}
     </div>
   );
