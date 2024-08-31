340e52df451bf97ddf44ba9a875048fddb7e8102
Franky Vong <franky.vong@gmail.com>
2024-08-31 21:37:12 +1000

    update

--- components/Problem.js
+++ components/Problem.js
@@ -44,10 +44,10 @@ const Problem = () => {
     <section className="bg-neutral text-neutral-content">
       <div className="max-w-7xl mx-auto px-8 py-16 md:py-32 text-center">
         <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight mb-6 md:mb-8">
-          80% of candidates don't know the answers that are important to interviewers
+          80% of candidates don&apos;t know the answers that are important to interviewers
         </h2>
         <p className="max-w-xl mx-auto text-lg opacity-90 leading-relaxed mb-12 md:mb-20">
-          Deals, comps, modelling... There's so much to learn.
+          Deals, comps, modelling... There&apos;s so much to learn.
         </p>
 
         <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-6">
